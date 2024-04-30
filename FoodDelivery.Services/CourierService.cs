256aa2a71dffb26078e9e3055eeaee00a14c6479
Volodymyr <vladymyrcherevko@gmial.com>
2024-04-30 18:14:14 +0300

    Added Courier service plug

---
+++ FoodDelivery.Services/CourierService.cs
@@ -0,0 +1,30 @@
+﻿using FoodDelivery.Contracts.Delivery;
+using FoodDelivery.Domain.Repositories;
+using FoodDelivery.Services.Abstractions;
+
+namespace FoodDelivery.Services;
+
+public class CourierService : ICourierService
+{
+	private readonly IRepositoryManager _repositoryManager;
+
+	public CourierService(IRepositoryManager repositoryManager)
+	{
+		_repositoryManager = repositoryManager;
+	}
+
+	public Task<DeliveryDto> GetDeliveryAsync(int courierId, int deliveryId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<DeliveryDto>> GetAvailableDeliveriesAsync(int courierId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> PickUpDeliveryAsync(int courierId, int deliveryId)
+	{
+		throw new NotImplementedException();
+	}
+}
